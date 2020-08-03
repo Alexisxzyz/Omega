dec331c3e4e986a875a5f2370e7c33ed3dc657c8
Alexisxzyz <69166204+Alexisxzyz@users.noreply.github.com>
2020-08-03 19:56:48 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
-// Initialize Firebase
-
-
-///Edit Under This
-
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyBN64wBeoNMAc_kmdJX7bwe5V6Q37hmMa0",
+  authDomain: "alexisxzyz.firebaseapp.com",
+  databaseURL: "https://alexisxzyz.firebaseio.com",
+  projectId: "alexisxzyz",
+  storageBucket: "alexisxzyz.appspot.com",
+  messagingSenderId: "417046719836",
+  appId: "1:417046719836:web:a664f0c0e38492efec6599",
+  measurementId: "G-1PQGLNF6CX"
 };
 
      
